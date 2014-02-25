ebd1a63163fabddc14225cb1a34bda350bb64b94
Matthijs Kooijman <matthijs@stdin.nl>
2014-02-25 10:21:22 +0100

    Add dummy lwm.h file for sketches to include
    This allows the IDE to detect the dependency, since it does not
    autodetect dependencies based on includes from subdirectories.

---
+++ src/lwm.h
@@ -0,0 +1,6 @@
+// Dummy file that can be included by sketches to let the Arduino IDE
+// autodetect the dependency on this library. It will then add the src/
+// directory to the include path, so sketches can include the specific
+// files they need. For example:
+// #include <lwm.h>
+// #include <lwm/nwk/nwk.h>
